4b9425d761ca9468e4c3f54875b60ba044a94b9a
notbeer <62919769+notbeer@users.noreply.github.com>
2021-07-13 23:57:12 -0400

    Compact version
    Previous one was messy cuz I just rushed through it lol

--- scripts/modules/minecraft/utils/misc/rainbowText.js
+++ scripts/modules/minecraft/utils/misc/rainbowText.js
@@ -1,19 +1,13 @@
-import { findTagOnPlayer } from '../others/cache.js';
+const rainbowCode = ['§4', '§c', '§6', '§e', '§g', '§2', '§a', '§b', '§3', '§9', '§5', '§d'];
 
-export function rainbowText(chatmsg) {
-    if(!findTagOnPlayer(chatmsg, 'rainbowText')) return; //This is how we check if the player has tag 'rainbowText'
-    const msgArray = chatmsg.message.split(' ');
-    const rainbowCode = ['§4', '§c', '§6', '§e', '§g', '§2', '§a', '§b', '§3', '§9', '§5', '§d'];
-    let newMessage = '';
-    let rainbowIndex = 0;
-    function generateRainbowText(text) {
-        text = text.replace(/§./g, '');
-        for(let i = 0; i < text.length; i++) {
-            newMessage += `${rainbowCode[rainbowIndex]}${text[i]}`;
+export function rainbowText(text) {
+    const letter = text.replace(/§./g, '').split('');
+    let newMessage = '', rainbowIndex = 0;
+    letter.forEach(letter => {
+        if(letter !== ' ') {
+            newMessage += `${rainbowCode[rainbowIndex]}${letter}`;
             rainbowIndex + 1 >= rainbowCode.length ? rainbowIndex = 0 : rainbowIndex++;
-        };
-        return newMessage = `${newMessage} `;
-    };
-    for(let i = 0; i < msgArray.length; i++) generateRainbowText(msgArray[i]);
-    return chatmsg.message = newMessage; //This will basically edit the players message with rainbow text and display the rainbow text in chat
+        } else newMessage += ' ';
+    })
+    return newMessage;
 };
