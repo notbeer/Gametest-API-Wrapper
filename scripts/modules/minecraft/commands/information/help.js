9b34494b9c69f0f4fa9c2d7809f474b458fdeb42
notbeer <62919769+notbeer@users.noreply.github.com>
2021-07-19 20:25:39 -0400

    Update help.js
    Edited a lot and added more explanation

--- scripts/modules/minecraft/commands/information/help.js
+++ scripts/modules/minecraft/commands/information/help.js
@@ -1,5 +1,6 @@
 import * as allCommands from '../export.js';
-import { config } from '../../index.js';
+import { config } from './../../index.js';
+import { runCommand } from '../../utils/others.js';
 
 var commandInfo = {
     cancelMessage: true,
@@ -11,17 +12,32 @@ var commandInfo = {
 };
 /**
  * Explanation of the parameters that are being passed in the 'execute' function
- * @param {Object} chatmsg This is the object that is passed by the event listening for messages being sent in chat
- * @param {Array} args This collectes all the message that comes after the prefix and the command name in a array, which is split by an 'space'
- * @param {Module} Minecraft This is the module Minecraft, which holds all the important classes. More information at: https://docs.microsoft.com/en-us/minecraft/creator/scriptapi/minecraft/minecraft
+ * @param {Object} chatmsg - This is the object that is passed by the event listening for messages being sent in chat
+ * @param {Array} args - This collectes all the message that comes after the prefix and the command name in a array, which is split by an 'space'
+ * @param {Module} Minecraft - This is the module Minecraft, which holds all the important classes. More information at: https://docs.microsoft.com/en-us/minecraft/creator/scriptapi/minecraft/minecraft
  */
 function execute(chatmsg, args, Minecraft) {
     const prefix = config.commandPrefix;
-    if(!args[0]) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"I have added a Rainbow Text in chat and Custom Commands.\nTo give your text Rainbow Effect in chat give yourself the tag 'rainbowText' (/tag @s add rainbowText)\n\nCustom Command prefix is: §a${prefix}§f\nHere are all the custom commands available...\n§aeval, help, location, home, ping\n§eType §a${prefix}help §b[command name]§e for more information on the command!\n\n§eThis pack was created by notbeer!\n§9Discord: §fnotbeer#9204\n§aXbox Live: §fColoringEmy86"}]}`);
+    const helpCmdList = ['help', 'eval', 'home'];
+    const features = [
+        '§f- §dSimple Command Handler and Custom Chat Commands - You can use AND make custom chat commands really easily with this pack.',
+        '§f- §dCustom Ranks - You can set custom ranks in chat next to your name! So, when you type in chat your rank will be displayed next to your name. To test this feature out for yourself execute the command: §a/tag @s add $(ChatRank{Rank-Name: OWNER})§r',
+        '§f- §dRainbow Text - Simply give yourself the tag \'rainbowText\' by executing the command §a/tag @s add rainbowText§d and then you type in chat your text should be all colorful!',
+        '§f- §dFloating Leaderboard - You can display the highest ranked player in a scoreboard using a floating text entity. Check out the function parameter comments for §a\'writeLeaderboard\' §din the file §autils/misc.js §dfor more information on how to get it setup.',
+        '§f- §d Useful Utils - I have also added a lot of useful utils. I will add more in the near future!'
+    ];
+
+    if(!args[0]) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§eGameTest FrameWork API Starter Pack§r\n§bCredit: §rThis pack was made by §anotbeer\n§9Discord: §fnotbeer#9204\n§aXbox Live: §fColoringEmy86\n\n§eFeatures: \n${features.join('\n')}\n\n§bCustom Command prefix§f: §a${prefix}\n§bCustom Command List: §l§c${helpCmdList.join(', ')}"}]}`);
+    
     let cmdList = allCommands[args[0]];
-    if(!cmdList) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cThat's not a valid command!"}]}`);
+    if(!cmdList) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cThat's not a valid command!"}]}`);
+    
     cmdList = cmdList.commandInfo;
-    Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§eCommand: §a${prefix}§f${args[0]}\n§eDescription: §f${cmdList.description}\n§aUsage: \n§a${prefix}§f${cmdList.usage.join(`\n§a${prefix}§f`)}"}]}`);
+    let infoArr = []
+    if(cmdList.description) infoArr.push(`§eDescription: §f${cmdList.description}`);
+    if(cmdList.usage.length) infoArr.push(`§eUsage: \n§f${cmdList.usage.join('\n')}`);
+
+    runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§eCommand: §a${prefix}§f${args[0]}\n${infoArr.join('\n')}"}]}`);
 };
 
 export { commandInfo, execute };
