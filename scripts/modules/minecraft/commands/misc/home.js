290650c4e183e07f797c90a468b90f4ef1a1b90b
notbeer <62919769+notbeer@users.noreply.github.com>
2021-07-19 20:26:09 -0400

    Better error handling

--- scripts/modules/minecraft/commands/misc/home.js
+++ scripts/modules/minecraft/commands/misc/home.js
@@ -1,3 +1,5 @@
+import { runCommand } from "../../utils/others.js";
+
 var commandInfo = {
     cancelMessage: true,
     description: 'Use this command to set home and later teleport to that position-',
@@ -10,12 +12,12 @@ var commandInfo = {
 };
 /**
  * Explanation of the parameters that are being passed in the 'execute' function
- * @param {Object} chatmsg This is the object that is passed by the event listening for messages being sent in chat
- * @param {Array} args This collectes all the message that comes after the prefix and the command name in a array, which is split by an 'space'
- * @param {Module} Minecraft This is the module Minecraft, which holds all the important classes. More information at: https://docs.microsoft.com/en-us/minecraft/creator/scriptapi/minecraft/minecraft
+ * @param {Object} chatmsg - This is the object that is passed by the event listening for messages being sent in chat
+ * @param {Array} args - This collectes all the message that comes after the prefix and the command name in a array, which is split by an 'space'
+ * @param {Module} Minecraft - This is the module Minecraft, which holds all the important classes. More information at: https://docs.microsoft.com/en-us/minecraft/creator/scriptapi/minecraft/minecraft
  */
 function execute(chatmsg, args, Minecraft) {
-    const data = Minecraft.Commands.run(`tag "${chatmsg.sender.name}" list`);
+    const data = runCommand(`tag "${chatmsg.sender.name}" list`).result;
     const coordFormat = /(?<=[x-zX-Z]: )(-\d+|\d+)/g;
     const homeName = args.slice(1).join(' ').toLowerCase();
     const homeRegex = new RegExp(`\\$\\(Home{Home-Name: \\b${homeName}\\b, X: (-\\d+|\\d+), Y: (-\\d+|\\d+), Z: (-\\d+|\\d+)(.*)}\\)`, 'g');
@@ -23,33 +25,33 @@ function execute(chatmsg, args, Minecraft) {
     const findHomeNames = /(?<=\(Home{Home-Name: ).+?(?=, X: (-\d+|\d+), Y: (-\d+|\d+), Z: (-\d+|\d+)}\))/g;
     const findXYZ = `${data.statusMessage.match(homeRegex)}`.match(coordFormat);
 
-    let listOptions = ['list', 'all'];let setOptions = ['set', 'add'];let removeOptions = ['remove'];let warpOptions = ['warp'];
+    let listOptions = ['list', 'all'];let setOptions = ['set', 'add'];let removeOptions = ['remove', 'unadd'];let warpOptions = ['warp', 'tp'];
     if(!args.length || listOptions.includes(args[0])) {
         const allHomes = data.statusMessage.match(findHomeNames);
-        return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"${allHomes ? `§bYou have total of §e${allHomes.length} §bhomes.\nHere are the list of homes: §a${allHomes.join('§r, §a')}` : '§cIt seems like you haven\'t set any home, yet-'}"}]}`);
+        return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"${allHomes ? `§bYou have total of §e${allHomes.length} §bhomes.\nHere are the list of homes: §a${allHomes.join('§r, §a')}` : '§cIt seems like you haven\'t set any home, yet-'}"}]}`);
     } else if(setOptions.includes(args[0])) {
-        if(!args[1]) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cPlease type a UNIQUE home name to set!"}]}`);
-        if(homeName.match(coordFormat)) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cYou may not indentify your home name in a coordinate format!"}]}`);
+        if(!args[1]) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cPlease type a UNIQUE home name to set!"}]}`);
+        if(homeName.match(coordFormat)) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cYou may not indentify your home name in a coordinate format!"}]}`);
 
-        if(data.statusMessage.match(homeRegex)) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cYou already have a home set with that name!"}]}`);
+        if(data.statusMessage.match(homeRegex)) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cYou already have a home set with that name!"}]}`);
 
-        Minecraft.Commands.run(`tag "${chatmsg.sender.name}" add "$(Home{Home-Name: ${homeName}, X: ${Math.trunc(chatmsg.sender.location.x)}, Y: ${Math.trunc(chatmsg.sender.location.y)}, Z: ${Math.trunc(chatmsg.sender.location.z)}})"`);
-        return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§bYou have set a home with the name §a${homeName} §bat§r: §a${Math.trunc(chatmsg.sender.location.x)}§r, §a${Math.trunc(chatmsg.sender.location.y)}§r, §a${Math.trunc(chatmsg.sender.location.z)}"}]}`);
+        runCommand(`tag "${chatmsg.sender.name}" add "$(Home{Home-Name: ${homeName}, X: ${Math.trunc(chatmsg.sender.location.x)}, Y: ${Math.trunc(chatmsg.sender.location.y)}, Z: ${Math.trunc(chatmsg.sender.location.z)}})"`);
+        return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§bYou have set a home with the name §a${homeName} §bat§r: §a${Math.trunc(chatmsg.sender.location.x)}§r, §a${Math.trunc(chatmsg.sender.location.y)}§r, §a${Math.trunc(chatmsg.sender.location.z)}"}]}`);
     } else if(removeOptions.includes(args[0])) {
-        if(!args[1]) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cPlease type a home name to remove!"}]}`);
-        if(!data.statusMessage.match(homeRegex)) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cYou don't have a home with that name!"}]}`);
+        if(!args[1]) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cPlease type a home name to remove!"}]}`);
+        if(!data.statusMessage.match(homeRegex)) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cYou don't have a home with that name!"}]}`);
         else {
             const tag = data.statusMessage.match(homeRegex);
-            Minecraft.Commands.run(`tag "${chatmsg.sender.name}" remove "${tag[0]}"`);
-            return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§bSuccessfully removed home with the name §a${homeName} §bat §a${findXYZ[0]}§r, §a${findXYZ[1]}§r, §a${findXYZ[2]}"}]}`);
+            runCommand(`tag "${chatmsg.sender.name}" remove "${tag[0]}"`);
+            return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§bSuccessfully removed home with the name §a${homeName} §bat §a${findXYZ[0]}§r, §a${findXYZ[1]}§r, §a${findXYZ[2]}"}]}`);
         };
     } else if(warpOptions.includes(args[0])) {
-        if(!args[1]) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cPlease type a home name to remove!"}]}`);
-        if(!data.statusMessage.match(homeRegex)) return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cYou don't have a home with that name!"}]}`);
+        if(!args[1]) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cPlease type a home name to remove!"}]}`);
+        if(!data.statusMessage.match(homeRegex)) return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cYou don't have a home with that name!"}]}`);
 
-        Minecraft.Commands.run(`execute "${chatmsg.sender.name}" ~~~ tp @s ${findXYZ[0]} ${findXYZ[1]} ${findXYZ[2]}`);
-        return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§bYou have been teleported to §a${findXYZ[0]}§r, §a${findXYZ[1]}§r, §a${findXYZ[2]}"}]}`);
-    } else return Minecraft.Commands.run(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cThat's not a valid argument!"}]}`);
+        runCommand(`execute "${chatmsg.sender.name}" ~~~ tp @s ${findXYZ[0]} ${findXYZ[1]} ${findXYZ[2]}`);
+        return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§bYou have been teleported to §a${findXYZ[0]}§r, §a${findXYZ[1]}§r, §a${findXYZ[2]}"}]}`);
+    } else return runCommand(`tellraw "${chatmsg.sender.name}" {"rawtext":[{"text":"§cThat's not a valid argument!"}]}`);
 };
 
 export { commandInfo, execute };
