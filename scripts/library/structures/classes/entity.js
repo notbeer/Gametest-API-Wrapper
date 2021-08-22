1c8e9f96eb26057df6b384009cf4cb755226f153
notbeer <62919769+notbeer@users.noreply.github.com>
2021-08-22 01:09:31 -0400

    getTags and findTag update

--- scripts/library/structures/classes/entity.js
+++ scripts/library/structures/classes/entity.js
@@ -3,19 +3,20 @@ import Server from "./server.js";
 class EntityBuilder {
     findTag(searchTag, target) {
         const allTags = this.getTags(target);
-        if(!allTags[0]) return false;
+        if(!allTags) return false;
         for(const tag of allTags) if(tag.replace(/§./g, '').match(new RegExp(`^${searchTag.replace(/§./g, '')}$`))) return true;
         return false;
     };
     getTags(target) {
         const data = Server.runCommand(`tag @e${target ? `[${target.replace(/\]|\[/g, '')}]` : ''} list`);
-        if(data.error) return [];
-        return data.result.statusMessage.match(/(?<=: ).*$/)[0].split('§r, §a');
+        if(data.error) return;
+        let tags = data.statusMessage.match(/(?<=: ).*$/);
+        if(tags) return tags[0].split('§r, §a');
     };
     getScore(objective, target, { minimum, maximum } = {}) {
         const data = Server.runCommand(`scoreboard players test @e${target ? `[${target.replace(/\]|\[/g, '')}]` : ''} ${objective} ${minimum ? minimum : '*'} ${maximum ? maximum : '*'}`);
         if(data.error) return;
-        return parseInt(data.result.statusMessage.match(/-?\d+/)[0]);
+        return parseInt(data.statusMessage.match(/-?\d+/)[0]);
     };
 };
 
